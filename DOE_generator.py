89053ec214cc85a718a28aff61753f0e7b1945d1
NguyeHe4 <henry.nguyen@lamresearch.com>
2023-02-24 08:41:54 -0800

    Update DOE_generator.py

--- DOE_generator.py
+++ DOE_generator.py
@@ -4,16 +4,29 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 import datetime
+import numpy as np
 
+# build.lhs({"Pressure":[50,70], "Temperature":[25,50], "Flow": [1000,2000]}, num_samples=12)
 
 doe_designs = [d for d in dir(build) if "build" not in d and "__" not in d]
-design = st.selectbox("DOE Design", options=doe_designs, index=doe_designs.index("full_fact"))
+design = st.selectbox(
+    "DOE Design", options=doe_designs, index=doe_designs.index("full_fact")
+)
 num_factors = int(st.number_input("Number of Factors", min_value=1))
 factors = {}
 for i in range(1, num_factors + 1):
     f = st.text_input(f"Factor {i}")
     levels = st.text_input(f"Level {i}")
-    factors[f] = eval(levels) if len(levels) > 0 and levels[0] == "["  else eval("[" + levels + "]")
+    factors[f] = (
+        eval(levels)
+        if len(levels) > 0 and levels[0] == "["
+        else eval("[" + levels + "]")
+    )
+    if type(factors[f][0]) == str:
+        try:
+            factors[f] = np.float(factors[f])
+        except:
+            pass
 
 function = eval(f"build.{design}")
 params = dict()
@@ -27,17 +40,20 @@ for param in list(func_params):
         param_input = st.text_input(param)
         if param_input != "":
             params[param] = param_input
+
+
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer = pd.ExcelWriter(output, engine="xlsxwriter")
+    df.to_excel(writer, index=False, sheet_name="Sheet1")
     workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
+    worksheet = writer.sheets["Sheet1"]
+    format1 = workbook.add_format({"num_format": "0.00"})
+    worksheet.set_column("A:A", None, format1)
     writer.save()
     processed_data = output.getvalue()
     return processed_data
+
 
 if factors != {}:
     DOE = eval("function(factors, **params)")
@@ -46,5 +62,9 @@ if factors != {}:
     DOE.rename(columns={DOE.columns[0]: "Run"}, inplace=True)
     st.dataframe(DOE)
     DOE_xlsx = to_excel(DOE)
-    st.download_button("Download DOE", data=DOE_xlsx, mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', file_name=f"{design}_{datetime.datetime.now().strftime(r'%Y-%m-%d %H-%M-%S')}.xlsx")
-
+    st.download_button(
+        "Download DOE",
+        data=DOE_xlsx,
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        file_name=f"{design}_{datetime.datetime.now().strftime(r'%Y-%m-%d %H-%M-%S')}.xlsx",
+    )
